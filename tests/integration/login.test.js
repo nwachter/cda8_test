f93ce83ffdf7d09f8d1d19ea27c4b04f02dbb2ef
nwachter <malaikaninaw@yahoo.fr>
2026-07-02 14:46:41 +0200

    test2

--- tests/integration/login.test.js
+++ tests/integration/login.test.js
@@ -1,12 +1,14 @@
 const request = require('supertest')
 const { app } = require('../../app')
 
-describe("Tester /login", async () => {
+describe("Tester /login", () => {
     it("Devrait retourner 200 si l'email contient @ et le pw est > 8 chars", async () => {
         const res = await request(app)
             .post('/login')
             .send({ email: "ninaw@gmail.com", password: "12345678" })
-            .expect(res.status).toBe(200)
-            .expect(res.body.message).toBe('Connexion Réussie !')
+
+        expect(res.status).toBe(200)
+        expect(res.body.message).toBe('Connexion réussie !')
     })
 })
+
